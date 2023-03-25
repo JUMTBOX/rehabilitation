2875a5df94d6c749a5956a11454a55c967724909
JUMTBOX <yjey12@naver.com>
2023-03-25 22:54:29 +0900

    ...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+import "./App.css";
+import axios from "axios";
+
+const getData = async () => {
+  const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+  const data = res.data;
+  // console.log(JSON.stringify(data[0].name));
+  return JSON.stringify(data);
+};
 
 function App() {
+  let [user, setUser] = useState("yang");
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form action="post">
+          <input
+            type="text"
+            name="username"
+            placeholder="insert username"
+          ></input>
+          <input
+            type="password"
+            name="password"
+            placeholder="insert password"
+          ></input>
+          <button type="submit">JOIN</button>
+        </form>
       </header>
+      <div></div>
     </div>
   );
 }
