31206fe13d7baad7814fbad40d8477251866e347
JUMTBOX <yjey12@naver.com>
2023-03-26 23:40:11 +0900

    udm addon

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
-import { useState, useEffect } from "react";
-import styled from "styled-components";
-import "./App.css";
-import axios from "axios";
-
-const getData = async () => {
-  const res = await axios.get("https://jsonplaceholder.typicode.com/users");
-  const data = res.data;
-  console.log(JSON.stringify(data[0].name));
-  return JSON.stringify(data);
-};
+// import { useState, useEffect } from "react";
+// import styled from "styled-components";
+// import axios from "axios";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  let [user, setUser] = useState("yang");
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <form action="post">
-          <input
-            type="text"
-            name="username"
-            placeholder="insert username"
-          ></input>
-          <input
-            type="password"
-            name="password"
-            placeholder="insert password"
-          ></input>
-          <button type="submit">JOIN</button>
-        </form>
-      </header>
-      <div>{user}</div>
+    <div>
+      <h1>let's get started!</h1>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
