3600bb5da7e1f2d40a4d0b0ffc584da016a1841b
JUMTBOX <yjey12@naver.com>
2023-06-10 20:16:19 +0900

    ...

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,16 +1,25 @@
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
+import { useState } from "react";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-      <button>Change Title</button>
+      <button
+        onClick={() => {
+          setTitle((cur) => "타이틀 바꾸기");
+        }}
+      >
+        Change Title
+      </button>
     </Card>
   );
 };
