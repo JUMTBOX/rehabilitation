0b91f2271454632222db8ba9c2170334136b5f6a
JUMTBOX <yjey12@naver.com>
2023-06-10 20:57:34 +0900

    ...

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,7 +3,11 @@ import React, { useState, useRef } from "react";
 const NewExpense = () => {
   const inputRef = useRef();
 
-  return <input type="text" ref={inputRef} />;
+  return (
+    <>
+      <input type="text" ref={inputRef} />
+    </>
+  );
 };
 
 export default NewExpense;
