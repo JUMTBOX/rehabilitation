15f0c4cde9847b696dc8f34943bcbc4760d509c9
JUMTBOX <yjey12@naver.com>
2023-06-14 18:48:10 +0900

    ...

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -3,15 +3,15 @@ import React from "react";
 import "./ExpensesFilter.css";
 
 const ExpensesFilter = () => {
-  const someThing = () => {
-    //
+  const dropdownChangehandler = (e) => {
+    console.log(e.target.value);
   };
 
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select onChange={someThing}>
+        <select onChange={dropdownChangehandler}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
