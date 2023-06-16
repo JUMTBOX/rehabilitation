05ac1dacea626399720a14532295262cb20b0fad
JUMTBOX <yjey12@naver.com>
2023-06-16 16:47:22 +0900

    ...

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -4,20 +4,21 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
+  // const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
-    setTitle((cur) => "updated!");
-  };
+  // const clickHandler = () => {
+  //   setTitle((cur) => "updated!");
+  // };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      <button>Change Title</button>
+      {/* onClick={clickHandler} */}
     </Card>
   );
 };
