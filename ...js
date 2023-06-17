6727fd6f1dbf14a0670765047e730fac9bc99d4d
JUMTBOX <yjey12@naver.com>
2023-06-17 22:18:38 +0900

    ...

---
+++ ...js
@@ -0,0 +1,13 @@
+// import axios from "axios";
+
+// async function loginRequest() {
+//   try {
+//     const loginData = await axios.post("http://localhost:3000/users/login", {
+//       id: ${userId},
+//       password: ${password},
+//     });
+//    loginData.status === 200 ? useNavigate("/main") : window.alert('login failed')
+//   } catch (err) {
+//     console.error(err, "problem with login request!!");
+//   }
+// }
